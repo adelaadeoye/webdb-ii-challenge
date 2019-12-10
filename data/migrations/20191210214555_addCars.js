e4fef511f7e6a7438044d8831e14a0d6deb0a7a2
adeoye adela <adeoye.adela@gmail.com>
2019-12-10 21:59:05 +0100

    added server and router

--- data/migrations/20191210214555_addCars.js
+++ data/migrations/20191210214555_addCars.js
@@ -1,8 +1,17 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable("cars", tbl => {
+        tbl.increments();
+
+        tbl.string("name", 255)
+            .notNullable()
+            .unique()
+            .index(); 
+
+        tbl.boolean("like_it").defaultTo(false);
+    });
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists("veggies");
 };
