c17c1edbaaa42422c31640ca0859a42e2cfdd8f8
adeoye adela <adeoye.adela@gmail.com>
2019-12-10 21:41:19 +0100

    initial commit

---
+++ knexfile.js
@@ -0,0 +1,31 @@
+/*
+
+this file contains instructions knex needs to connect
+to the database
+
+Each key on the exported object has the configuration
+for a database environment (dev, production, testing, etc)
+
+The knex CLI will use the configuration on this file to find the DB.
+
+The knex CLI will use the "development" environment by default, but
+we can override that behavior.
+
+*/
+
+module.exports = {
+  // teaches knex to connect to the development database
+  development: {
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./data/cars.db3"
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  }
+};
